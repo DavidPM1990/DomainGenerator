6ca035386faf33e2f90c998a27a5b566c192d49b
David Pardo <davidpardomartin@gmail.com>
2024-01-08 20:46:08 +0100

    script domain generator made

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io"];
+
+  let domainNames = [];
+
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let domainName =
+            pronouns[i] + adjectives[j] + nouns[k] + extensions[l];
+
+          domainNames.push(domainName);
+        }
+      }
+    }
+  }
+
+  console.log("el array con los dominios----->", domainNames);
+
+  // for (let m = 0; m < domainNames.length; m++) {
+  //     console.log("los dominios-------->", domainNames[m]);
+  // }
+
   console.log("Hello Rigo from the console!");
 };
